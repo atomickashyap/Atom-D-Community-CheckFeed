d7a5ac4a166c4c128974dbaf96a95b106f31123a
AmanKas <aman312000@gmail.com>
2023-03-13 16:46:33 +0530

    Phase 1 Check Main

---
+++ index.js
@@ -0,0 +1,19 @@
+//sidebar
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(chat => {
+        let name=chat.querySelector('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1){
+            chat.style.display = 'flex';
+        } else{
+            chat.style.display = 'none';
+        }
+    })
+}
+
+messageSearch.addEventListener('keyup', searchMessage);
+
